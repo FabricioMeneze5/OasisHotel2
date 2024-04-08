5f777d3ea00d87391f9fbdb21635b27fd4a66d3c
Fabricio Menezes <fabricio-menezes@live.com>
2024-04-08 20:55:51 +0100

    ADD IMAGES config

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
+// SASS config
 function styles() {
     return gulp.src('./src/styles/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = gulp.parallel(styles);
+//IMAGES config
+function images() {
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
