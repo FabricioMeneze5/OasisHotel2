34b2cc78f26942d4a9a5018d67add58e3358d2df
Fabricio Menezes <fabricio-menezes@live.com>
2024-04-17 11:33:17 +0100

    ADD buttons next/prev

--- src/main.js
+++ src/main.js
@@ -1,23 +1,43 @@
-//Select imgs and bg
-var thumbnails = document.querySelectorAll('.img-thumbnail');
-var displayedImg = document.querySelector('#img-displayed');
+//Select imgs, buttons and bg
+var displayedImg = document.querySelector('#displayed__img');
 var background = document.querySelector('#background');
+var prevButton = document.querySelector('#prev-button');
+var nextButton = document.querySelector('#next-button');
+
+var thumbnails = Array.from(document.querySelectorAll('.img-thumbnail')); //select and take all images to array
+var displayed = document.querySelector('.displayed'); //select displayer
+var currentIndex = 0; // Variable to store the index of the currently displayed image
+
+//show image in your display
+function displayImage(index) {
+    displayedImg.src = thumbnails[index].src;
+    displayed.style.display = 'block';
+    background.style.display = 'block';
+}
 
 // Add a event listener for each mini img
-thumbnails.forEach(function(thumbnail) {
+thumbnails.forEach(function(thumbnail, index) {
     thumbnail.addEventListener('click', function() {
-        // Show background and displayed img 
-        displayedImg.style.display = 'block';
-        background.style.display = 'block';
-
-        // Set src to displayed img
-        displayedImg.src = this.src;
+        currentIndex = index;
+        displayImage(currentIndex);
     });
+});
+
+// Event listener prev button
+prevButton.addEventListener('click', function() {
+    currentIndex = (currentIndex - 1 + thumbnails.length) % thumbnails.length;
+    displayImage(currentIndex);
+});
+
+// Event listener next button
+nextButton.addEventListener('click', function() {
+    currentIndex = (currentIndex + 1) % thumbnails.length;
+    displayImage(currentIndex);
 });
 
 // Close img and bg
 background.addEventListener('click', function() {
     // Hide background and displayed img 
-    displayedImg.style.display = 'none';
+    displayed.style.display = 'none';
     this.style.display = 'none';
 });
