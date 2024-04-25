7b1bd4c52ad649be11e045ec01e097cf1803a133
Fabricio Menezes <fabricio-menezes@live.com>
2024-04-25 22:12:29 +0100

    ADD carousel

--- src/script/main.js
+++ src/script/main.js
@@ -12,3 +12,14 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+
+let counter = 1;
+setInterval(function() {
+    const currentSlide = document.getElementById('slide' + counter).checked = true;
+    counter ++;
+
+    if (counter > 5) {
+        counter = 1;
+    }
+}, 3000);
